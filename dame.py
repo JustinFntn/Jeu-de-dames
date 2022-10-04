c6e90357fd629f1a56bac96a04ecb8f305459d0b
Fontaine <jufo6216@gmail.com>
2022-10-04 11:14:41 +0200

    init_damier+menu+entrer_coordonnee fonctionnel

--- dame.py
+++ dame.py
@@ -3,12 +3,28 @@
 #####################
 
 import numpy as np
-import string
 
-# création du damier
 # le jeu se passe sur les lignes 1 à 10 et colonnes 1 à 10
 # les lignes 0 et 11 et les colonnes 0 et 11 utilisées pour l'affichage
 n = 12
+
+# class pion:
+#     def __init__(self,couleur,emplacement):
+#         self.couleur=couleur
+#         self.emplacement=emplacement
+
+# sous-programme
+
+
+def menu():
+    # menu simple avec question pour jouer ou non
+    select = " "
+    print("voulez-vous jouer une partie? (oui/non)")
+    select = input(select)
+    while (select != "oui" and select != "non"):
+        print('veuillez saisir "oui" ou "non"')
+        select = input(select)
+    return select
 
 
 def init_damier():
@@ -16,22 +32,65 @@ def init_damier():
     dim = [[" "]*n for i in range(n)]
     tableau = np.array(dim)  # tableau de dimension 12 avec des " "
     for i in range(1, n-1):
-        tableau[i][0] = i-1
-        tableau[i][n-1] = i-1
-        tableau[0][i] = chr(96+i)
-        tableau[n-1][i] = chr(96+i)
+        tableau[i][0] = chr(96+i)
+        tableau[i][n-1] = chr(96+i)
+        tableau[0][i] = i-1
+        tableau[n-1][i] = i-1
     for i in range(1, 5):
         tableau[i][range(1+(i % 2), n-1, 2)] = "n"
     for i in range(7, n-1):
         tableau[i][range(1+(i % 2), n-1, 2)] = "b"
     return tableau
 
-# def avancer_pion(lettre, numer):
-#     return lettre+numero
+
+def pion_valide_joueur(damier, pion, joueur):
+    # Vérifie si le pion pointé est correcte selon le joueur
+    if (pion[0] < 1 or pion[0] > 10 or pion[1] < 1 or pion[1] > 10):
+        return False
+    elif (joueur == 0 and damier[pion[0]][pion[1]] == 'b'):
+        return True
+    elif (joueur == 1 and damier[pion[0]][pion[1]] == 'n'):
+        return True
+    else:
+        return False
 
 
+def entrer_coordonnee(cas):
+    # Afficher une demande de saisie de coordonnées avec des cas différents selon la situation
+    if (cas == 1):
+        valeur_entree = input(
+            "saisir les coordonnées du pion à déplacer (exemple a6):")
+        destination = [int(ord(valeur_entree[0]))-96, int(valeur_entree[1])+1]
+    elif (cas == 2):
+        valeur_entree = input(
+            "coordonnées incorrect, veuillez saisir des coordonnées :")
+        destination = [int(ord(valeur_entree[0]))-96, int(valeur_entree[1])+1]
+    elif (cas == 3):
+        valeur_entree = input(
+            "saisir les coordonnées de destination :")
+        destination = [int(ord(valeur_entree[0]))-96, int(valeur_entree[1])+1]
+    return destination
+
+
+def deplacement_pion(damier, joueur):
+    # l'emplacement de départ et arrivé sont dans le damier
+    # l'emplacement d'arrivé est en diagonale de l'emplacement de départ
+    depart = entrer_coordonnee(1)
+    while (pion_valide_joueur(damier, depart, joueur) == False):
+        depart = entrer_coordonnee(2)
+    arrive = entrer_coordonnee(3)
+    while (damier[arrive[0]][arrive[1]] != " "):
+        arrive = entrer_coordonnee(2)
+
+    return 0
+
+
+    # prog principal
 if __name__ == "__main__":
+    joueurB = 0
+    joueurN = 1
+    # selection = menu()
     damier = init_damier()
     print(damier)
-
-# print(avancer_pion(3.2, 3.2))
+    a = deplacement_pion(damier, joueurN)
+    print(a)
